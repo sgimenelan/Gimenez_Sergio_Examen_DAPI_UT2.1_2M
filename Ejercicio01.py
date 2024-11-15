47029d59a953cbbfd15df55789bcbad0068dc0fd
sgimenelan <sgimenelan@educacion.navarra.es>
2024-11-15 09:21:39 +0100

    Create Ejercicio01.py

---
+++ Ejercicio01.py
@@ -0,0 +1,5 @@
+comida = input("Dime tu comida favorita: ")
+print("Me encanta", comida, "tambien!")
+edad = int(input("Dime tu edad: "))
+meses = edad * 12
+print("Has vivido aproximadamente", meses, "meses")
